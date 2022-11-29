d5e1199733c489ddca9dea6acdd05a0a1c299b8f
tumble-weed <aniketsinghresearch@gmail.com>
2022-11-29 14:32:26 +0000

    for virtualenv

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ virtualenv .
 source bin/activate
 pip install -r requirements.txt
 apt-get install ffmpeg libsm6 libxext6  -y
+# export LD_LIBRARY_PATH=/kaggle/working/msg-gan-v1/lib/python3.7/site-packages/nvidia/cublas/lib/:$LD_LIBRARY_PATH
