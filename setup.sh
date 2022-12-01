e63ed36332518644f6c1b0dbd133263df4dcd146
tmuble-weed <aniketsinghresearch@gmail.com>
2022-12-01 09:52:08 +0000

    download_images

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ pip3 install virtualenv
 pip install -r requirements.txt
 apt-get install ffmpeg libsm6 libxext6  -y
 # export LD_LIBRARY_PATH=/kaggle/working/msg-gan-v1/lib/python3.7/site-packages/nvidia/cublas/lib/:$LD_LIBRARY_PATH
+bash download_images.sh
